c1c24c0575efa7d48aeeb0d0f315e4929a328f02
bacon <dbech22@student.aau.dk>
2026-03-05 15:43:04 +0100

    Test of CalculateSpawn

--- Core/Spawning/City.cs
+++ Core/Spawning/City.cs
@@ -2,11 +2,14 @@ namespace Core.Spawning;
 
 using Core.Shared;
 
-public class City(string name, float spawnChance, Position position, int population)
+public class City(string name, Position position, int population, float spawnChance = 0.0f)
 {
     public readonly int Id;
     public readonly string Name = name;
-    public readonly float SpawnChance = spawnChance;
-    public readonly int Population = population;
+
     public readonly Position Position = position;
+
+    public readonly int Population = population;
+
+    public float SpawnChance = spawnChance;
 }
