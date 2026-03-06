b1e346c99f859b5079021c1e4f73ee59d7ddb3e3
Anders Mariegaard <74593851+AndersMariegaard@users.noreply.github.com>
2026-03-06 15:40:17 +0100

    Price_distribution for stations (#23)
    * Added a static energyprices class to store hourly prices, a getter for these prices. Extended station to have a price and a setter with +- 20% deviation.
    
    * Added unit tests
    
    * Added Math.Random as an injected dependency.
    
    Added GetDayPrice getter.
    Added DaysOfWeek enum.
    
    * Correctly included fees in interpolated data.
    
    * Moved the energy price table to csv, and added dependency injection to the path.

--- Core/Charging/Station.cs
+++ Core/Charging/Station.cs
@@ -1,16 +1,49 @@
 namespace Core.Charging;
 
 using Core.Shared;
+using System;
 
+/// <summary>
+/// An EV charging station.
+/// </summary>
+/// <param name="id">The id of the station.</param>
+/// <param name="name">The name of the station, e.g. 'OK Aarselv, Logistikparken'.</param>
+/// <param name="address">The physical address of the station, e.g. 'Logistikparken 12'.</param>
+/// <param name="position">Longitude/Latitude of the station.</param>
+/// <param name="chargers">A list of the chargers attached to the station.</param>
+/// <param name="price">The KWh price of charging at the station.</param>
+/// <param name="random">An injected instance of Math.Random.</param>
 public class Station(ushort id,
-               string name,
-               string address,
-               Position position,
-               List<Charger>? chargers)
+                string name,
+                string address,
+                Position position,
+                List<Charger>? chargers,
+                float price,
+                Random random)
 {
+
+    /// <summary>The current KWh price at the station.</summary>
+    public float Price = price;
+    private readonly Random _random = random;
     private readonly ushort _id = id;
     private readonly string _name = name;
     private readonly string _address = address;
     public readonly Position Position = position;
     private readonly List<Charger>? _chargers = chargers;
+
+    /// <summary>
+    /// Calculates the price of a specific station.
+    /// </summary>
+    /// <param name="hour">The hour being queried.</param>
+    /// <remarks>
+    /// The new price is randomly generated in the range [3.0, 5.0].
+    /// Call this periodically to simulate dynamic pricing.
+    /// </remarks>
+    public void CalculatePrice(DayOfWeek day = DayOfWeek.Monday, int hour = 12)
+    {
+        var basePrice = EnergyPrices.GetHourPrice(day, hour);
+        var deviation = 0.10f + (_random.NextSingle() * 0.10f); // 10–20%
+        var sign = _random.Next(2) == 0 ? 1.0f : -1.0f;
+        Price = basePrice * (1.0f + (sign * deviation));
+    }
 }
