9b14808548f87f036b8bf1f75b69b0e280b40768
Niklas Bo Hedehus <niklas.hedehus@gmail.com>
2026-03-20 16:16:22 +0100

    Added further description to Bechs comments

--- Engine/Events/Events.cs
+++ Engine/Events/Events.cs
@@ -1,31 +1,61 @@
 namespace Engine.Events;
+// ----------- DOMAIN EVENTS ----------- //
 
-// 1+QueueSize_E, Produce Arrival Event (Actual arrival time * +- 20%), Path Deviation
-// Metrics: Count of reservation requests and timestamps
+// Functionality: 
+//  - Should increase Expected Queue Size by 1 (EQS + 1).
+//  - Produce an arrival event where the expected arrival time is Actual_Arrival_Time * +- 20%.
+//  - Calculate what the path deviation to the station will be from the original journey.
+// Metrics:
+//  - Count of reservation requests and their timestamps for when the reservation requests are made.
 public readonly record struct ReservationRequest(uint EVId, ushort StationId, int Time) : IEvent;
 
-// -1 QueueSize_E, Path Deviation, Sample once from urgency)
-// Metrics: Count of cancellation requests and timestamps
+// Functionality: 
+//  - Should decrease Expected Queue Size by 1 (EQS - 1).
+//  - Calculate the EVs path deviation of the original journey from its current position.
+//  - Get its own urgency and sample once from the urgency graph.
+// Metrics:
+//  - Count of cancellation requests and their timestamps for when the cancellation requests are made.
 public readonly record struct CancelRequest(uint EVId, ushort StationId, int Time) : IEvent;
 
-// 1+QueueSize_A, Place EV in queue or start charging immediately, Store arrival time
+// Functionality: 
+//  - Should increase Actual Queue Size by 1 (AQS + 1).
+//  - Method for either placing the EV in the queue or if there are no EVs in the queue, immediately start charging.
+//  - Method that stores the EVs arrival time at the station.
 public readonly record struct ArriveAtStation(uint EVId, ushort StationId, int Time) : IEvent;
 
-//public readonly record struct StartCharging(uint EVId, int ChargerId, int Time) : IEvent;
-
-// Pop next EV from queue and start charging, store start time, Recompute Integration
-// Metrics: Time spent charging
+// Functionality: 
+//  - Pop next EV from the the queue and start charging that EV and stores the EV's start charging time.
+//  - Integrate the Recompute functionality.
+// Metrics:
+//  - Record the time an EV spent charging
 public readonly record struct EndCharging(uint EVId, int ChargerId, int Time) : IEvent;
 
-// Metrics: Missed deadlines/Arrival vs Expected arrival time, Path Deviation
+// Functionality: 
+//  - Pop next EV from the the queue and start charging that EV and stores the EV's start charging time.
+//  - Integrate the Recompute functionality.
+// Metrics:
+//  - Record if the EV missed its deadline.
+//  - Record how much the EV missed the deadline by.
+//  - Record the path deviation of an EVs actual journey compared to its original journey.
 public readonly record struct ArriveAtDestination(uint EVId, int Time) : IEvent;
 
 
-//Non-domain events
+// ---------- NON-DOMAIN EVENTS ---------- //
 
-//Spawn (Spawn EV and Sample once from urgency)
+// Spawn
+// Functionality:
+//  - Spawn an EV .
+//  - Sample once from urgency to see if it needs to find a charger immediately.
 
-//Snapshot (iterate over stations and ...)
-//Metrics: Utilization, queue size, price, number/% of active chargers
+// Snapshot
+// Functionality:
+//  - Iterate over stations to collect metrics.
+// Metrics:
+//  - Record utilisation of station
+//  - Record average queue size
+//  - Record average price
+//  - Record number of active chargers in percentages% 
 
-//Check urgency (Intercal based on car SoC)
+// Check urgency
+// Functionality:
+//  - Method that checks the urgency for an interval that is based on some car SoC, like every 10% or so.
