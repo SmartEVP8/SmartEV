43dbd6499edfcc2571a374c0862a80f684f0404e
bacon <dbech22@student.aau.dk>
2026-03-23 13:22:17 +0100

    Ints

--- Engine/Events/Events.cs
+++ Engine/Events/Events.cs
@@ -9,41 +9,41 @@ public abstract record CancelableEvent(int EVId, Time Time) : Event(Time);
 
 // ----------- DOMAIN EVENTS ----------- //
 
-// Functionality: 
+// Functionality:
 //  - Should increase Expected Queue Size by 1 (EQS + 1).
 //  - Produce an arrival event where the expected arrival time is Actual_Arrival_Time * +- 20%.
 //  - Calculate what the path deviation to the station will be from the original journey.
 // Metrics:
 //  - Count of reservation requests and their timestamps for when the reservation requests are made.
-public record ReservationRequest(uint EVId, ushort StationId, Time Time) : Event(Time);
+public record ReservationRequest(int EVId, ushort StationId, Time Time) : Event(Time);
 
-// Functionality: 
+// Functionality:
 //  - Should decrease Expected Queue Size by 1 (EQS - 1).
 //  - Calculate the EVs path deviation of the original journey from its current position.
 //  - Get its own urgency and sample once from the urgency graph.
 // Metrics:
 //  - Count of cancellation requests and their timestamps for when the cancellation requests are made.
-public record CancelRequest(uint EVId, ushort StationId, Time Time) : Event(Time);
+public record CancelRequest(int EVId, ushort StationId, Time Time) : Event(Time);
 
-// Functionality: 
+// Functionality:
 //  - Should increase Actual Queue Size by 1 (AQS + 1).
 //  - Method for either placing the EV in the queue or if there are no EVs in the queue, immediately start charging.
 //  - Method that stores the EVs arrival time at the station.
-public record ArriveAtStation(uint EVId, ushort StationId, Time Time) : CancelableEvent(EVId, Time);
+public record ArriveAtStation(int EVId, ushort StationId, Time Time) : CancelableEvent(EVId, Time);
 
-// Functionality: 
+// Functionality:
 //  - Pop next EV from the the queue and start charging that EV and stores the EV's start charging time.
 //  - Integrate the Recompute functionality.
 // Metrics:
 //  - Record the time an EV spent charging
-public record EndCharging(uint EVId, int ChargerId, Time Time) : CancelableEvent(EVId, Time);
+public record EndCharging(int EVId, int ChargerId, Time Time) : CancelableEvent(EVId, Time);
 
 
 // Metrics:
 //  - Record if the EV missed its deadline.
 //  - Record how much the EV missed the deadline by.
 //  - Record the path deviation of an EVs actual journey compared to its original journey.
-public record ArriveAtDestination(uint EVId, Time Time) : CancelableEvent(EVId, Time);
+public record ArriveAtDestination(int EVId, Time Time) : CancelableEvent(EVId, Time);
 
 
 // ---------- NON-DOMAIN EVENTS ---------- //
