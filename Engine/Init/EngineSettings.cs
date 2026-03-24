cf3c0a7efc5f9ffcfe316d33828fb7df98d81173
MertzAndreas <Andreasmertz11@gmail.com>
2026-03-24 14:29:49 +0100

    Bech changes

--- Engine/Init/EngineSettings.cs
+++ Engine/Init/EngineSettings.cs
@@ -12,7 +12,6 @@ public class EngineSettings
     required public Random Seed { get; init; }
     required public StationFactoryOptions StationFactoryOptions { get; init; }
 
-    required public int MaximumEVs { get; init; }
     required public int CurrentAmoutOfEVsInDenmark { get; init; }
     required public int IntervalToCheckUrgency { get; init; }
     required public uint ChargingStepSeconds { get; init; }
