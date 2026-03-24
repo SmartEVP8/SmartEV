fccf55e8388106a1bba83d50d2c8b4a66d77ec27
Anders Mariegaard <Andersmariegaard@gmail.com>
2026-03-24 13:40:20 +0100

    Initial cost function

--- Core/Routing/Journey.cs
+++ Core/Routing/Journey.cs
@@ -16,14 +16,16 @@ public class Journey(Time departure, Time originalDuration, Paths path)
     public Time Departure { get; } = departure;
 
     /// <summary>
-    /// Gets the original duration of the journey.
+    /// Gets the original duration of the journey, i.e. the duration of A -> B without any detours.
     /// </summary>
     public Time OriginalDuration { get; } = originalDuration;
 
     /// <summary>
-    /// Gets the path of the journey.
+    /// Gets the path as it is currently. 
+    /// This can be updated as the journey progresses, e.g. if the EV is rerouted to a different station.
+    /// Represented by waypoints that are mutated as the journey progresses.
     /// </summary>
-    public Paths Path { get; } = path;
+    public Paths Path { get; private set; } = path;
 
     private float _runningSumDeviation;
 
@@ -91,4 +93,10 @@ public class Journey(Time departure, Time originalDuration, Paths path)
     /// <summary> Updates the running sum deviation for this journey.</summary>
     /// <param name="deviation">The new deviation to set.</param>
     public void UpdateRunningSumDeviation(float deviation) => _runningSumDeviation = deviation;
+
+    /// <summary>
+    /// Updates the path of the journey. This can be used to update the path as the journey progresses, e.g. if the EV is rerouted to a different station.
+    /// </summary>
+    /// <param name="newPath">The new path for the journey.</param>
+    public void UpdatePath(Paths newPath) => Path = newPath;
 }
--- Engine/Cost/ComputeCost.cs
+++ Engine/Cost/ComputeCost.cs
@@ -1,8 +1,73 @@
 namespace Engine.Cost;
 
+using System.Data;
+using Core.Charging;
+using Core.Shared;
+using Core.Vehicles;
+using Engine.Routing;
+
 /// <summary>
-/// Provides methods for computing costs related to various operations and decisions within the Engine, such as routing, charging, and scheduling.
+/// Computes the cost of detouring to each station and selects the station with the lowest cost.
 /// </summary>
-public static class ComputeCost
+/// <param name="costStore">The cost store.</param>
+/// <param name="pathDeviator">The path deviator.</param>
+public class ComputeCost(ICostStore costStore, PathDeviator pathDeviator)
 {
+    private readonly ICostStore _costStore = costStore;
+    private readonly PathDeviator _pathDeviator = pathDeviator;
+
+    /// <summary>
+    /// Computes the cost of detouring to each station and selects the station with the lowest cost.
+    /// </summary>
+    /// <param name="ev">The EV for which to compute costs.</param>
+    /// <param name="stations">The array of stations to evaluate.</param>
+    /// <param name="journeys">The journeys for each station.</param>
+    /// <param name="currentTime">The current time.</param>
+    /// <returns>The station with the lowest cost.</returns>
+    /// <exception cref="NoNullAllowedException">If no suitable station is found.</exception>
+    public Station Compute(ref EV ev, Station[] stations, (float[] duration, float[] distance, string[] polyline) journeys, Time currentTime)
+    {
+        var totalCost = 0d;
+        Station? bestStation = null;
+
+        for (var i = 0; i < stations.Length; i++)
+        {
+            var tempCost = 0d;
+            var station = stations[i];
+            var (duration, distance, polyline) = (journeys.duration[i], journeys.distance[i], journeys.polyline[i]);
+
+            // Factors
+            var effectiveQueueSize = station.Chargers.Sum(c => c.Queue.Count);
+            var (totalChargers, availableChargers) = (
+                station.Chargers.Count,
+                station.Chargers.Where(c => c.Queue.Count == 0).ToList().Count
+            );
+            var availableChargerRatio = MathF.Abs((availableChargers / totalChargers) - 1);
+            var pathDeviation = _pathDeviator.CalculateDetourDeviation(ev.Journey, currentTime, station.Position);
+            var urgency = Urgency.CalculateChargeUrgency(ev.Battery.StateOfCharge, ev.Preferences.MinAcceptableCharge);
+            var price = station.Price;
+
+            // Costs
+            var weights = _costStore.GetWeights();
+
+            var effectiveQueueCost = weights.EffectiveQueueSize * MathF.Pow(effectiveQueueSize, 2);
+            var pathDeviationCost = weights.PathDeviation * pathDeviation.Item1;
+            var urgencyCost = weights.Urgency * urgency;
+            var priceCost = weights.PriceSensitivity * ev.Preferences.PriceSensitivity * price;
+            var effectiveWaitTimeCost = weights.ExpectedWaitTime * 0; // TODO: Compute expected wait time cost
+            var availableChargerRatioCost = weights.AvailableChargerRatio * availableChargerRatio;
+
+            tempCost = effectiveQueueCost + pathDeviationCost + urgencyCost + priceCost + effectiveWaitTimeCost + availableChargerRatioCost;
+            if (tempCost > totalCost)
+            {
+                totalCost = tempCost;
+                bestStation = station;
+            }
+        }
+
+        if (bestStation is null)
+            throw new NoNullAllowedException("No station found in station map.");
+
+        return bestStation;
+    }
 }
