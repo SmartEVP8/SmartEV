3e757ac2bdc2dcd7406a5995ff40527095178eea
MertzAndreas <Andreasmertz11@gmail.com>
2026-03-24 14:09:05 +0100

    Bench

--- Benchmark/Engine.Benchmark/Program.cs
+++ Benchmark/Engine.Benchmark/Program.cs
@@ -7,9 +7,10 @@ public static class Program
 {
     public static async Task Main()
     {
-        BenchmarkRunner.Run<StationsAroundPolyline>();
-        BenchmarkRunner.Run<Polyline6Decode>();
-        BenchmarkRunner.Run<Polyline6DecodeParallel>();
-        BenchmarkRunner.Run<OsrmRouterBenchmark>();
+        // BenchmarkRunner.Run<StationsAroundPolyline>();
+        // BenchmarkRunner.Run<Polyline6Decode>();
+        // BenchmarkRunner.Run<Polyline6DecodeParallel>();
+        // BenchmarkRunner.Run<OsrmRouterBenchmark>();
+        BenchmarkRunner.Run<OsrmRouterOneToManyBenchmark>();
     }
 }
