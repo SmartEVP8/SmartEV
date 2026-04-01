fcfaad3c1123fe21415503769d67d70e93264a0b
Anders Mariegaard <74593851+AndersMariegaard@users.noreply.github.com>
2026-04-01 09:55:21 +0200

    Cleaned up indexed srcToDest query (#173)
    * Added test to make sure osrm returns the same across queries
    
    * Removed for loop from index query
    
    * Fixed test
    
    * Cleaned up function signatures in OSRMRouter

--- Benchmark/Engine.Benchmark/OsrmRouterBenchmark.cs
+++ Benchmark/Engine.Benchmark/OsrmRouterBenchmark.cs
@@ -27,8 +27,8 @@ public class OsrmRouterBenchmark
     {
         var path = AppContext.GetData("OsrmDataPath") as string
             ?? throw new InvalidOperationException("OsrmDataPath not set in project.");
-
-        var csvPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "energy_prices.csv");
+        var csvPath = AppContext.GetData("EnergyPricesPath") as string
+            ?? throw new InvalidOperationException("EnergyPricesPath not set in project.");
         var energyPrices = new EnergyPrices(new FileInfo(csvPath), new Random(42));
 
         var stations = new List<Station>(50);
@@ -85,5 +85,65 @@ public class OsrmRouterBenchmark
     {
         var (lon, lat) = _evCoordinates[0];
         _ = _router.QuerySingleDestination(lon, lat, _stationCoordsFlat[0], _stationCoordsFlat[1]);
+    }
+
+    /// <summary>
+    /// Benchmarks querying a single destination.
+    /// </summary>
+    [Benchmark]
+    public void QueryDestinationEVtoDest()
+    {
+        var (lon, lat) = _evCoordinates[0];
+        _ = _router.QuerySingleDestination(lon, lat, _stationCoordsFlat[0], _stationCoordsFlat[1]);
+    }
+
+    /// <summary>
+    /// Benchmarks querying a single destination with a pre-indexed station for snapping.
+    /// </summary>
+    [Benchmark]
+    public void QueryDestinationEVtoDestWithStation()
+    {
+        var (lon, lat) = _evCoordinates[0];
+        _ = _router.QueryDestinationWithStop(lon, lat, _stationCoordsFlat[0], _stationCoordsFlat[1], _stationCoordsFlat[2], _stationCoordsFlat[3], _stationIndices[0]);
+    }
+
+    /// <summary>
+    /// Benchmarks querying 1000 EVs each routing through a pre-indexed station to a destination.
+    /// The station is looked up by pre-computed index with cached snapping hints.
+    /// </summary>
+    [Benchmark]
+    public void QueryWithIndexedStation1000Evs()
+    {
+        var random = new Random(42);
+        for (var i = 0; i < _evCoordinates.Length; i++)
+        {
+            var (lon, lat) = _evCoordinates[i];
+            var stationIdx = (ushort)random.Next(0, 50);
+            var destIdx = (ushort)random.Next(0, 50);
+            while (destIdx == stationIdx)
+                destIdx = (ushort)random.Next(0, 50);
+
+            _ = _router.QueryDestinationWithStop(lon, lat, _stationCoordsFlat[stationIdx * 2], _stationCoordsFlat[(stationIdx * 2) + 1], _stationCoordsFlat[destIdx * 2], _stationCoordsFlat[(destIdx * 2) + 1], stationIdx);
+        }
+    }
+
+    /// <summary>
+    /// Benchmarks querying 1000 EVs each routing through the same stations to the same destinations.
+    /// The station is snapped at query time (no pre-computed hints).
+    /// </summary>
+    [Benchmark]
+    public void QueryWithQueryTimeSnappedStation1000Evs()
+    {
+        var random = new Random(42);
+        for (var i = 0; i < _evCoordinates.Length; i++)
+        {
+            var (lon, lat) = _evCoordinates[i];
+            var stationIdx = (ushort)random.Next(0, 50);
+            var destIdx = (ushort)random.Next(0, 50);
+            while (destIdx == stationIdx)
+                destIdx = (ushort)random.Next(0, 50);
+
+            _ = _router.QueryDestinationWithStop(lon, lat, _stationCoordsFlat[stationIdx * 2], _stationCoordsFlat[(stationIdx * 2) + 1], _stationCoordsFlat[destIdx * 2], _stationCoordsFlat[(destIdx * 2) + 1], ushort.MaxValue);
+        }
     }
 }
